308c3197faf405a96876691a7de7f8feb16cd186
Abram Felix <abramfelix1@gmail.com>
2023-04-22 09:05:15 -0700

    add get request for /bookings/current to get all bookings of current user, add logic to setPreview helper function to check for arrays

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -6,6 +6,66 @@ const { requireAuth } = require("../../utils/auth");
 const {} = require("../../utils/validation");
 const sequelize = require("sequelize");
 const { Op } = require("sequelize");
-const {} = require("../../utils/helpers");
+const { setPreview } = require("../../utils/helpers");
+
+/* Get All Bookings of Current */
+router.get("/current", async (req, res, next) => {
+  const { user } = req;
+  const bookings = await Booking.findAll({
+    where: {
+      userId: user.dataValues.id,
+    },
+    include: [
+      {
+        model: Spot,
+        include: [
+          {
+            model: Image,
+            as: "previewImage",
+            where: { preview: 1 },
+            attributes: ["url"],
+          },
+        ],
+        attributes: { exclude: ["description", "createdAt", "updatedAt"] },
+      },
+    ],
+    attributes: [
+      "id",
+      "spotId",
+      "userId",
+      "startDate",
+      "endDate",
+      "createdAt",
+      "updatedAt",
+    ],
+  });
+
+  const buildBookings = [];
+  for (const i in bookings) {
+    const {
+      id,
+      spotId,
+      Spot,
+      userId,
+      startDate,
+      endDate,
+      createdAt,
+      updatedAt,
+    } = bookings[i].dataValues;
+    setPreview(Spot);
+    buildBookings[i] = {
+      id,
+      spotId,
+      Spot,
+      userId,
+      startDate,
+      endDate,
+      createdAt,
+      updatedAt,
+    };
+  }
+
+  res.json({ Bookings: buildBookings });
+});
 
 module.exports = router;
