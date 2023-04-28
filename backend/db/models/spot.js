6adeb6819df57028967e160f4c71f1ab8af7479d
Abram Felix <abramfelix1@gmail.com>
2023-04-27 19:43:41 -0700

    update

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -119,6 +119,7 @@ module.exports = (sequelize, DataTypes) => {
                   ],
                 ],
               },
+              subQuery: true,
               ...extras,
             };
           }
