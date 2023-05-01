3de89326b2cbf67a34e1e83981c850cb5a5e52a2
Abram Felix <abramfelix1@gmail.com>
2023-05-01 11:36:25 -0700

    add group to get user spots

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -212,7 +212,7 @@ router.get("/current", requireAuth, async (req, res, next) => {
         required: false,
       },
     ],
-    group: ["Spot.id", "PreviewImage.id"],
+    group: ["Spot.id", "previewImage.id"],
   });
 
   if (!spots.length) {
