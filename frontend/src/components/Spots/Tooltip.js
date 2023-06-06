d73538359c96132656fe1f9d364a9a47569e7143
Abram Felix <abramfelix1@gmail.com>
2023-06-06 16:55:48 -0700

    add tooltip

--- frontend/src/components/Spots/Tooltip.js
+++ frontend/src/components/Spots/Tooltip.js
@@ -4,7 +4,9 @@ const Tooltip = ({ text, children }) => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
   const handleMouseMove = (e) => {
-    setPosition({ x: e.clientX, y: e.clientY });
+    const tooltipRef = document.getElementById("tooltip");
+    const tooltipWidth = tooltipRef?.offsetWidth;
+    setPosition({ x: e.clientX - tooltipWidth / 2 + 3, y: e.clientY + 32 });
   };
 
   return (
@@ -16,8 +18,9 @@ const Tooltip = ({ text, children }) => {
       {children}
       {position.x !== 0 && position.y !== 0 && (
         <div
+          id="tooltip"
           className="tooltip"
-          style={{ left: position.x - 55, top: position.y + 35 }}
+          style={{ left: position.x, top: position.y }}
         >
           {text}
         </div>
