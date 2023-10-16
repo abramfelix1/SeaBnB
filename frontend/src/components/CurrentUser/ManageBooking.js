13968323f88754632699a470b939ecd165f13d0c
Abram Felix <abramfelix1@gmail.com>
2023-10-16 02:44:10 -0700

    style bookings

--- frontend/src/components/CurrentUser/ManageBooking.js
+++ frontend/src/components/CurrentUser/ManageBooking.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import ReviewsList from "../Reviews/ReviewsList";
 import { useDispatch, useSelector } from "react-redux";
 import { getCurrentBookings } from "../../store/booking";
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 
 export default function ManageBooking() {
   const dispatch = useDispatch();
@@ -38,7 +39,40 @@ export default function ManageBooking() {
         </div>
         <div className="bookings-list-container">
           {bookings.map((booking, idx) => (
-            <div>A</div>
+            <div key={idx} className="booking-item">
+              <NavLink to={`/spots/${booking.Spot.id}`}>
+                <img
+                  src={booking.Spot.previewImage}
+                  alt={booking.Spot.name}
+                  className="booking-image"
+                ></img>
+              </NavLink>
+              <NavLink to={`/spots/${booking.Spot.id}`}>
+                <p className="booking-name">{booking.Spot.name}</p>
+              </NavLink>
+              <p className="booking-location">
+                {booking.Spot.address},{booking.Spot.city}
+              </p>
+              <p className="booking-location">${booking.Spot.price}/night</p>
+              <p className="booking-dates">
+                {booking.startDate.split("T")[0]} -{" "}
+                {booking.endDate.split("T")[0]}
+              </p>
+              <div className="current-buttons-container">
+                <button
+                  className="edit-button"
+                  // onClick={() => handleEdit(booking)}
+                >
+                  Update
+                </button>
+                <button
+                  className="delete-button"
+                  // onClick={() => handleDelete(booking)}
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
           ))}
         </div>
       </div>
